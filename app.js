5fc3f02421872cc4ce8db123d2eca6867813e020
KCordilia <k.cordilia@hotmail.com>
2019-04-10 12:01:57 +0200

    edit capability

--- app.js
+++ app.js
@@ -5,26 +5,40 @@ document.addEventListener("DOMContentLoaded", () => {
     const input = document.querySelector("#myInput");
     const ul = document.getElementsByTagName("ul")[0];
     const li = document.createElement("li");
+    const span = document.createElement("span");
     const removeBtn = document.createElement("span");
     const editBtn = document.createElement('button');
 
     if (input.value == "") {
       alert("You have to type something!");
     } else {
-      li.textContent = input.value;
+      span.textContent = input.value;
       input.value = "";
       removeBtn.className = "close";
       removeBtn.textContent = "\u00D7";
       ul.appendChild(li);
+      li.appendChild(span);
       li.appendChild(removeBtn);
       li.appendChild(editBtn);
       editBtn.className = 'edit';
       editBtn.textContent = 'Edit';
+      
 
       removeBtn.addEventListener("click", () => {
         const ul = document.getElementsByTagName("ul")[0];
         const li = document.querySelector("li");
         ul.removeChild(li);
+      });
+
+      editBtn.addEventListener("click", () => {
+        const span = li.firstElementChild;
+        const input = document.createElement('input');
+        input.type = 'text';
+
+        li.insertBefore(input, span);
+        li.removeChild(span);
+
+
       });
     }
   });
